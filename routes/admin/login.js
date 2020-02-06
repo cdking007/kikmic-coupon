2bda292ef85cdb1beb3e89c3bd4a6b51dffa8309
Chirag Pipaliya <chiragpipaliya454@gmail.com>
2020-02-06 23:14:34 +0530

    minor bug fixxed in login

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -14,7 +14,7 @@ router.get("/login", ensureNotAuthenticated, (req, res, next) => {
     thumbUrl: "https://kikmic.ca/wp-content/uploads/2019/04/cropped-mini.png"
   });
 });
-router.post("/login", (req, res, next) => {
+router.post("/login", ensureNotAuthenticated, (req, res, next) => {
   passport.authenticate("local", {
     successRedirect: "/admin/coupons",
     failureRedirect: "/login",
@@ -31,7 +31,7 @@ router.get("/signup", ensureNotAuthenticated, (req, res) => {
   });
 });
 
-router.post("/signup", async (req, res) => {
+router.post("/signup", ensureNotAuthenticated, async (req, res) => {
   const { email, username, password } = { ...req.body };
   const admin = new Admin({ email, username, password });
   await admin
